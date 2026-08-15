7e384ea23be12f5a698021312264d4bc33b862a4
PaulTiffany <paulctiffany@gmail.com>
2026-08-14 20:58:31 -0400

    Expose AlphaClaw public API

---
+++ src/alphaclaw/__init__.py
@@ -0,0 +1,23 @@
+from .core import AlphaClaw
+from .model import (
+    AlphaState,
+    Ambiguity,
+    Claim,
+    EvidenceRef,
+    IngressRequest,
+    Observation,
+    SourceQuery,
+)
+from .provider import MultimodalProvider
+
+__all__ = [
+    "AlphaClaw",
+    "AlphaState",
+    "Ambiguity",
+    "Claim",
+    "EvidenceRef",
+    "IngressRequest",
+    "MultimodalProvider",
+    "Observation",
+    "SourceQuery",
+]
